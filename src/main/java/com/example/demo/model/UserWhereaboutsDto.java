1247b3a8cbab4482a37379e28fe15fc4509de546
Antonov333 <antonovsergei333@yahoo.com>
2024-05-18 15:42:26 +0300

    Created UserWhereaboutsDto.java (dummy at the moment), advanced with UserWhereabouts and UserWhereaboutsService.java

---
+++ src/main/java/com/example/demo/model/UserWhereaboutsDto.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+/**
+ * DTO for user whereabouts
+ * <br>Dummy at the moment, while it could contain fields of phone numbers, addresses, emails, etc.
+ */
+public class UserWhereaboutsDto {
+}
