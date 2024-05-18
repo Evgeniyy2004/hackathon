5a81a413da5daae4dd14c62c9e019494393f17e0
Evgeniy Zig <evgeniy.ziganshin.04@mail.ru>
2024-05-18 14:14:50 +0500

    start ig

--- src/main/java/com/example/demo/DataController.java
+++ src/main/java/com/example/demo/DataController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+
 @Controller
 @RequestMapping("/get/{id}/{password}")
 public class DataController {
@@ -15,23 +17,41 @@ public class DataController {
     @Autowired
     private WithdrawService service1;
 
-    @Autowired
-    private DepositService service2;
 
     @Autowired
-    private UserService service3;
+    private UserService service2;
 
     @GetMapping("/{period}")
-    public String getAll(Model model,@PathVariable("id") String userId,
+    public String getAll(Model model, @PathVariable("id") String userId,
                          @PathVariable("password") String password, @PathVariable("period") Period period) {
-        if (!service3.get(userId,password)){
+        if (!service2.get(userId, password)) {
             return "unregistered";
         }
 
 
-        var withdraws = service1.get(userId,period);
-        model.addAttribute()
-        var deposits = service2.get(userId,period);
+        var all = service1.get(userId, period);
+        long plus = 0;
+        long minus = 0;
+
+        HashMap<Category, Long> withdraws = new HashMap();
+        for (DTO d : all) {
+            if (d.type == WITHDRAW) {
+                if (withdraws.containsKey(d.category)) {
+                    withdraws.put(d.category, withdraws.get(d.category) + d.sum);
+                } else {
+                    withdraws.put(d.category, d.sum);
+                }
+                plus +=d.sum;
+            } else minus += d.sum;
+        }
+        model.addAttribute("balance", plus - minus);
+        if (period == Period.WEEK){
+            model.addAttribute("period","31");
+        }
+        else {
+            model.addAttribute("period","7");
+        }
+        model.addAttribute("withdraws",withdraws);
         return "information";
     }
 }
---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/{id}/{password}")
+public class UserController {
+
+}
