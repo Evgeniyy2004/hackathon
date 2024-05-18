287fdaad26ad69021954e9657dc61abcd07c45e8
evgen <evgeniy.ziganshin.04@mail.ru>
2024-05-18 14:43:41 +0500

    nah

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,10 +1,23 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user/{id}/{password}")
 public class UserController {
+    @Autowired
+    private UserService service;
 
+
+
+    @PostMapping("/")
+    public String register(Model model, @PathVariable("id") String userId,
+                           @PathVariable("password") String password){
+            if (service.get(userId,password))
+    }
 }
