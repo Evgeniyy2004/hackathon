3ce2595a3fc668fbd6083f75ccaec2b8557e8433
Antonov333 <antonovsergei333@yahoo.com>
2024-05-18 16:08:24 +0300

    Created BankOperationDto and BankOperationCategory enum

---
+++ src/main/java/com/example/demo/model/BankOperationCategory.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+/**
+ * Category of bank transaction
+ */
+public enum BankOperationCategory {
+
+    /**
+     * Receiving of funds
+     */
+    INCOME,
+
+    /**
+     * some expense of funds
+     */
+    WITHDRAWAL
+}
